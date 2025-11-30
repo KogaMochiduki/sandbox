584c917c6f2523bc085feaad3a817a78cbd81955
KogaMochiduki <b1646038@planet.kanazawa-it.ac.jp>
2025-11-30 22:27:27 +0900

    add: capture a flight recorder snapshot

--- go/guessNumber/v2/server/main.go
+++ go/guessNumber/v2/server/main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
+	"runtime/trace"
 	"strconv"
 	"sync"
 	"time"
@@ -19,10 +21,17 @@ type bucket struct {
 }
 
 func main() {
+	// Set up the flight recorder
+	fr := trace.NewFlightRecorder(trace.FlightRecorderConfig{
+		MinAge:   200 * time.Millisecond,
+		MaxBytes: 1 << 20, // 1 MiB
+	})
+	fr.Start()
+
 	// Make one bucket for each valid number a client could guess.
 	// The HTTP handler will look up the guessed number in buckets by
 	// using the number as an index into the slice.
-	buckets := make([]bucket, 100)
+	buckets := make([]bucket, 2000)
 
 	// Every minute, we send a report of how many times each number was guessed.
 	go func() {
@@ -54,7 +63,13 @@ func main() {
 		// Respond to the client with the guess and whether it was correct.
 		fmt.Fprintf(w, "guess: %d, correct: %t", guess, guess == answer)
 
-		log.Printf("HTTP request: endpoint=/guess-number guess=%d duration=%s answer=%d", guess, time.Since(start), answer)
+		// Capture a snapshot if the response takes more than 100ms.
+		// Only the first call has any effect.
+		if fr.Enabled() && time.Since(start) > 10*time.Millisecond {
+			go captureSnapshot(fr)
+		}
+
+		log.Printf("HTTP request: endpoint=/guess-number guess=%d duration=%s", guess, time.Since(start))
 	})
 	log.Fatal(http.ListenAndServe(":8090", nil))
 }
@@ -81,4 +96,30 @@ func sendReport(buckets []bucket) {
 	if _, err := http.Post(url, "application/json", bytes.NewReader(b)); err != nil {
 		log.Printf("failed to send report: %s", err)
 	}
+}
+
+var once sync.Once
+
+// captureSnapshot captures a flight recorder snapshot.
+func captureSnapshot(fr *trace.FlightRecorder) {
+	// once.Do ensures that the provided function is executed only once.
+	once.Do(func() {
+		f, err := os.Create("snapshot.trace")
+		if err != nil {
+			log.Printf("opening snapshot file %s failed: %s", f.Name(), err)
+			return
+		}
+		defer f.Close() // ignore error
+
+		// WriteTo writes the flight recorder data to the provided io.Writer.
+		_, err = fr.WriteTo(f)
+		if err != nil {
+			log.Printf("writing snapshot to file %s failed: %s", f.Name(), err)
+			return
+		}
+
+		// Stop the flight recorder after the snapshot has been taken.
+		fr.Stop()
+		log.Printf("captured a flight recorder snapshot to %s", f.Name())
+	})
 }
